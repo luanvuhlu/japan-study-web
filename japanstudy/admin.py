fb2b2c4d816106fb75fe723807b89a660f71d933
Luan Vu <luanvuhlu@gmail.com>
2015-06-05 00:09:22 +0700

    Add test word

--- japanstudy/admin.py
+++ japanstudy/admin.py
@@ -1,24 +1,23 @@
 from django.contrib import admin
-from models import Tag, JapaneseWord
+from models import Tag, JapaneseWord, TestWord
 from django.utils import timezone
 
 class BaseAdmin(admin.ModelAdmin):
     def delete_model(self, request, obj):
         obj.active='N'
+        obj.save()
 class JapaneseWordAdmin(BaseAdmin):
-    list_display=('source', 'mean', 'created_time')
+    list_display=('source', 'kanji','mean', 'other_mean', 'level', 'created_time', 'active')
     readonly_fields=('user', 'created_time')
     date_hierarchy='created_time'
     list_filter=('created_time', )
-    search_fields=['source', 'mean', 'created_time']
+    search_fields=['source', 'mean', 'kanji', 'other_mean', 'created_time']
+    ordering = ('-active', 'level', 'created_time', )
     def save_model(self, request, obj, form, change):
         if not obj.id:
-            obj.created_time=timezone.now()
             obj.user=request.user
         obj.save()
-    def delete_model(self, request, obj):
-        super(JapaneseWordAdmin, self).delete_model()
-        obj.save()
+
 class TagAdmin(BaseAdmin):
     list_display=('name', 'created_time')
     readonly_fields=('user', 'created_time')
@@ -27,12 +26,21 @@ class TagAdmin(BaseAdmin):
     search_fields=['name', 'created_time']
     def save_model(self, request, obj, form, change):
         if not obj.id:
-            obj.created_time=timezone.now()
             obj.user=request.user
         obj.save()
-    def delete_model(self, request, obj):
-        super(TagAdmin, self).delete_model()
+
+class TestWordAdmin(BaseAdmin):
+    list_display=('start_date', 'completed_time')
+    readonly_fields=('user', 'created_time')
+    date_hierarchy='created_time'
+    list_filter=('created_time', )
+    search_fields=['start_date', 'created_time', 'completed_time']
+    def save_model(self, request, obj, form, change):
+        if not obj.id:
+            obj.user=request.user
         obj.save()
+
 # Register your models here.
 admin.site.register(Tag, TagAdmin)
 admin.site.register(JapaneseWord, JapaneseWordAdmin)
+admin.site.register(TestWord, TestWordAdmin)
--- japanstudy/models.py
+++ japanstudy/models.py
@@ -6,21 +6,23 @@ STATUS=(('Y', 'Y'), ('N', 'N'))
 JAPANESE_WORD_TYPE=((True, 'Hiragana'), (False, 'Katakana'))
 class Tag(models.Model):
     name=models.CharField(max_length=100, blank=False, verbose_name="Tên")
-    description=models.TextField(max_length=500, blank=True, verbose_name="Mô tả")
+    description=models.TextField(max_length=500, null=True, blank=True, verbose_name="Mô tả")
     active=models.CharField(max_length=2, choices=STATUS, default='Y', verbose_name="Trạng thái")
-    created_time=models.DateTimeField(blank=True, verbose_name="Ngày tạo")
+    created_time=models.DateTimeField(auto_now_add=True, blank=True, verbose_name="Ngày tạo")
     user=models.ForeignKey(User, verbose_name="Người tạo")
     def __unicode__(self):
         return self.name
 class Word(models.Model):
     source=models.CharField(max_length=200, blank=False, verbose_name="Từ")
     mean=models.CharField(max_length=200, blank=False, verbose_name="Nghĩa")
+    other_mean=models.TextField(max_length=500, null=True, blank=True, verbose_name="Nghĩa khác")
     level=models.PositiveSmallIntegerField(validators =[MinValueValidator(0)], default=0, blank=False, verbose_name="Độ khó")
-    description=models.TextField(max_length=500, blank=True, verbose_name="Mô tả")
+    description=models.TextField(max_length=500, null=True, blank=True, verbose_name="Mô tả")
+    example=models.TextField(max_length=500, null=True, blank=True, verbose_name="Ví dụ")
     user=models.ForeignKey(User, verbose_name="Người tạo")
     tags=models.ManyToManyField(Tag, blank=True, verbose_name="Tag")
     sames=models.ManyToManyField('self', blank=True, verbose_name="Từ cùng nghĩa")
-    created_time=models.DateTimeField(blank=True, verbose_name="Ngày tạo")
+    created_time=models.DateTimeField(auto_now_add=True, blank=True, verbose_name="Ngày tạo")
     active=models.CharField(max_length=2, choices=STATUS, default='Y', verbose_name="Trạng thái")
     def __unicode__(self):
         return self.source
@@ -28,8 +30,10 @@ class JapaneseWord(Word):
     kanji=models.CharField(max_length=200, blank=True, verbose_name="Từ Kanji")
     type=models.BooleanField(default=True, choices=JAPANESE_WORD_TYPE, verbose_name="Loại từ", blank=False)
 class TestWord(models.Model):
+    title=models.CharField(max_length=200, null=True, verbose_name="Tiêu đề best")
     words=models.ManyToManyField(Word, blank=False, verbose_name="Gồm các từ")
-    start_date=models.DateField(blank=True, verbose_name="Ngày bắt đầu")
-    completed_time=models.DateTimeField(blank=True, verbose_name="Ngày hoàn thành")
-    created_time=models.DateTimeField(blank=True, verbose_name="Ngày tạo")
+    start_date=models.DateTimeField(null=True, blank=True, verbose_name="Ngày bắt đầu")
+    completed_time=models.DateTimeField(null=True, blank=True, verbose_name="Ngày hoàn thành")
+    created_time=models.DateTimeField(auto_now_add=True, blank=True, verbose_name="Ngày tạo")
+    user=models.ForeignKey(User, verbose_name="Người tạo")
     active=models.CharField(max_length=2, choices=STATUS, default='Y', verbose_name="Trạng thái")
